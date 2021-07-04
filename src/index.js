be18fd7d602633d8731dad130ac854a9486e4087
orangefruit21 <orangecat21@yandex.ru>
2021-07-04 15:45:05 +0300

    feat: walking character

--- src/index.js
+++ src/index.js
@@ -1,58 +1,33 @@
 import './index.scss';
+import characterSprite from './assets/Female-2-Walk.png';
 
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
-ctx.beginPath();
-ctx.moveTo(50, 50);
-ctx.lineTo(550, 50);
-ctx.lineTo(550, 550);
-ctx.lineTo(50, 550);
+// Ширина и высота кадра на спрайте
+const spriteW = 48;
+const spriteH = 48;
+// Количество кадров
+const shots = 3;
+// Текущий кадр
+let cycle = 0;
 
-ctx.fillStyle = 'red';
-ctx.lineWidth = 10;
-ctx.closePath();
-ctx.stroke();
-ctx.fill();
+// Время последнего рендера (мс)
+let lastRender = 0;
 
-ctx.beginPath();
-ctx.arc(300, 400, 100, 0, 3.14);
-ctx.stroke();
+const img = document.createElement('img');
+img.src = characterSprite;
 
-ctx.beginPath();
-ctx.arc(150, 200, 50, 0, 3.14);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.arc(450, 200, 50, 0, 3.14);
-ctx.stroke();
-
-let isOpen = false;
-
-const openCloseEye = () => {
-  if (isOpen) {
-    ctx.clearRect(95, 145, 110, 55);
-    ctx.fillRect(95, 145, 110, 55);
-
-    ctx.clearRect(395, 145, 110, 55);
-    ctx.fillRect(395, 145, 110, 55);
-
-    ctx.clearRect(195, 295, 210, 105);
-    ctx.fillRect(195, 295, 210, 105);
-  } else {
-    ctx.beginPath();
-    ctx.arc(150, 200, 50, Math.PI, 2 * Math.PI);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.arc(450, 200, 50, Math.PI, 2 * Math.PI);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.arc(300, 400, 100, Math.PI, 2 * Math.PI);
-    ctx.stroke();
+const drawShot = (timestamp) => {
+  if (!lastRender || timestamp - lastRender >= 0.1 * 1000) {
+    lastRender = timestamp;
+    cycle = (cycle + 1) % shots;
+    ctx.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight);
+    ctx.drawImage(img, cycle * spriteW, 0, spriteW, spriteH, 0, 0, 100, 100);
   }
-  isOpen = !isOpen;
+  window.requestAnimationFrame(drawShot);
 };
 
-setInterval(openCloseEye, 1000);
+img.addEventListener('load', () => {
+  drawShot();
+});
